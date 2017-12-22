947473690e6f902fb39eafce565a7e09c4227678
苏 <enmoton@qq.com>
2017-12-22 12:25:28 +0800

    Reflect - 添加ClassInfo类

---
+++ Reflect/src/main/java/ClassInfo.java
@@ -0,0 +1,37 @@
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+public class ClassInfo implements Serializable
+{
+    public static void main(String[] args)
+    {
+    }
+
+    @Getter
+    @Setter
+    private int pv;
+
+    protected int po;
+
+    public int pu;
+
+    public static int ps;
+
+    public static void psMethod()
+    {
+    }
+
+    public void puMethod()
+    {
+    }
+
+    protected void poMethod()
+    {
+    }
+
+    private void pvMethod()
+    {
+    }
+}
