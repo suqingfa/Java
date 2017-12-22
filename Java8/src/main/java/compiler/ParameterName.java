0c0951a68d2c959b08826bdf61990bec3417d3ef
苏 <enmoton@qq.com>
2017-12-22 18:01:19 +0800

    Java8 - 参数名称

---
+++ Java8/src/main/java/compiler/ParameterName.java
@@ -0,0 +1,24 @@
+package compiler;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+
+public class ParameterName
+{
+    public static void main(String[] args)
+    {
+        /**
+         * java8可以通过Parameter.getName()方法来获取参数名
+         * 但是要在编译时带上-parameters参数
+         */
+
+        for (Method method : ParameterName.class.getDeclaredMethods())
+        {
+            System.out.println(method.toGenericString());
+            for (Parameter parameter : method.getParameters())
+            {
+                System.out.println(" Parameter: " + parameter.getName());
+            }
+        }
+    }
+}
