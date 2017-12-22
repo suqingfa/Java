b10d81ffb3aecf1cd9550bc83db931afd4ad19e4
苏 <enmoton@qq.com>
2017-12-22 12:32:50 +0800

    Reflect - 获取类的内部类外部类，实现的接口

--- Reflect/src/main/java/ClassInfo.java
+++ Reflect/src/main/java/ClassInfo.java
@@ -8,6 +8,17 @@ public class ClassInfo implements Serializable
     public static void main(String[] args)
     {
         Class clazz = ClassInfo.class;
+
+        // 获取类的内部类
+        clazz.getDeclaredClasses();
+        // 获取类的外部类
+        clazz.getDeclaringClass();
+
+        // 获取类实现的接口
+        for (Class interfaceClass : clazz.getInterfaces())
+        {
+            System.out.println(interfaceClass.toGenericString());
+        }
     }
 
     @Getter
