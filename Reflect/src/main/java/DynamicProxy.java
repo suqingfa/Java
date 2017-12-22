b77e3e4aa5e9971af4b6a122bcd31d5d760d6bed
苏 <enmoton@qq.com>
2017-12-22 15:03:44 +0800

    Reflect - 使cglib实现aop

--- Reflect/src/main/java/DynamicProxy.java
+++ Reflect/src/main/java/DynamicProxy.java
@@ -1,3 +1,6 @@
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+
 import java.lang.reflect.*;
 
 public class DynamicProxy implements InvocationHandler
@@ -12,6 +15,31 @@ public class DynamicProxy implements InvocationHandler
 
         doSomething.doSomething();
         doSomething.toString();
+
+        // CGLIB（Code Generation Library）实现 AOP
+        Enhancer enhancer = new Enhancer();
+        // 设置被代理类
+        enhancer.setSuperclass(Superclass.class);
+        // 设置代理的拦截器
+        enhancer.setCallback((MethodInterceptor) (obj, method, args1, proxy) ->
+        {
+            System.out.println(" Before invoke");
+            System.out.println(" Invoke method: " + method.toGenericString());
+            proxy.invokeSuper(obj, args1);
+            System.out.println(" After invoke");
+            return null;
+        });
+        Superclass inst = (Superclass) enhancer.create();
+        System.out.println("Enhancer:");
+        inst.invoke();
+    }
+
+    static class Superclass
+    {
+        void invoke()
+        {
+            System.out.println(" invoke");
+        }
     }
 
     @Override
