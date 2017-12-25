4e6f1dcf87257a3af668430b49385bf227393da3
苏 <enmoton@qq.com>
2017-12-25 13:33:37 +0800

    Reflect - 反射效率测试

---
+++ Reflect/src/main/java/Main.java
@@ -0,0 +1,20 @@
+import java.lang.reflect.Constructor;
+
+public class Main
+{
+    public static void main(String[] args) throws Exception
+    {
+        // 反射效率测试
+
+        Constructor<Integer> constructor = Integer.class.getDeclaredConstructor(Integer.TYPE);
+        // 取消了Java的权限控制检查。禁用后，在java8中提高约10%。
+        constructor.setAccessible(true);
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < Integer.MAX_VALUE; i++)
+        {
+            constructor.newInstance(i);
+        }
+        System.out.println("Time : " + (System.currentTimeMillis() - start));
+    }
+}
